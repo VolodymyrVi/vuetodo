36c7e38721a00f15b8d171f868acc05457e65677
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-08 23:13:33 +0300

    added TodoComponent.vue

---
+++ resources/js/components/TodoComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
